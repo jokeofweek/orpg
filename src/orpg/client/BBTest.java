6899a15c33bdf01474137c61ed9c432146e42afa
jokeofweek <dominiccharleyroy@gmail.com>
2013-02-01 10:24:25 -0500

    Filled out bytestream. Currently supports strings, booleans, and primitive integer types (no floating points yet).

--- src/orpg/client/BBTest.java
+++ src/orpg/client/BBTest.java
@@ -1,9 +1,6 @@
 package orpg.client;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.nio.charset.Charset;
 import java.util.Arrays;
 
 import orpg.shared.ByteStream;
@@ -11,11 +8,14 @@ import orpg.shared.ByteStream;
 public class BBTest {
 
 	public static void main(String[] args) throws IOException {
-		byte[] bytes = ByteStream.serialize((byte)1, true, "Hello, world.", false);
-		
+		byte[] bytes = ByteStream.serialize((byte) 1, true,
+				"Hello, world!", Byte.MIN_VALUE, Short.MAX_VALUE,
+				Integer.MAX_VALUE, Long.MAX_VALUE, false, "Goodbye, world...");
 		bytes = Arrays.copyOfRange(bytes, 3, bytes.length);
-		Object[] objects = ByteStream.unserialize(bytes, Boolean.class, String.class, Boolean.class);
-		
+		Object[] objects = ByteStream.unserialize(bytes, Boolean.class,
+				String.class, Byte.class, Short.class, Integer.class,
+				Long.class, Boolean.class, String.class);
+
 		for (Object o : objects) {
 			System.out.println(o);
 		}
--- src/orpg/shared/ByteStream.java
+++ src/orpg/shared/ByteStream.java
@@ -4,88 +4,146 @@ import java.nio.charset.Charset;
 import java.util.Arrays;
 
 public class ByteStream {
-	
+
 	public static final String CHARSET_NAME = "UTF-8";
 	private static final Charset CHARSET = Charset.forName(CHARSET_NAME);
 
-	public static Object[] unserialize(byte[] data, Class... classes) throws ArrayIndexOutOfBoundsException {
+	/**
+	 * This unserializes a byte array which has been serialized with
+	 * {@link ByteStream#serialize(byte, Object...)}. Note that it does not
+	 * return the type of the packet (stored in the header byte) and thus this
+	 * should be extracted manually.
+	 * 
+	 * @param data
+	 *            the serialized byte array.
+	 * @param classes
+	 *            the classes of the objects in the byte array, stored in the
+	 *            same order as they were seralized in.
+	 * @return an array of the objects extracted from the byte array. The
+	 *         classes of these objects will match the <i>clases</i> parameter
+	 *         if the unserialization was successful.
+	 * 
+	 * @throws ArrayIndexOutOfBoundsException
+	 *             if the byte array is not formatted correctly. This usually
+	 *             means there wa an error in formatting the packet, the classes
+	 *             were sent wrong, or the byte array was tampered with.
+	 * @throws IllegalArgumentException
+	 *             if the number of classes passed mismatches the number of
+	 *             objects in the byte array
+	 * @throws SerializationException
+	 *             if a class passed was not a serializable type.
+	 * 
+	 */
+	public static Object[] unserialize(byte[] data, Class... classes)
+			throws ArrayIndexOutOfBoundsException {
 		int offset = 0;
-		
+
 		// Get first byte for number of objects
 		Object[] objects = new Object[readByte(data, offset)];
 		offset++;
-		
+
 		// If we have a mismatch between object count and class count.
 		if (objects.length != classes.length) {
-			throw new IllegalArgumentException("Class count does not match object count.");
+			throw new IllegalArgumentException(
+					"Class count does not match object count.");
 		}
-		
+
 		for (int i = 0; i < objects.length; i++) {
-			System.out.println(offset + "," + data.length);
+			// Deserialize based on type
 			if (classes[i] == Byte.class) {
 				objects[i] = readByte(data, offset);
 			} else if (classes[i] == Short.class) {
 				objects[i] = readShort(data, offset);
 			} else if (classes[i] == Integer.class) {
-				//objects[i] = readInteger(data, offset);
+				objects[i] = readInteger(data, offset);
 			} else if (classes[i] == Long.class) {
-				// objects[i] = readLong(data, offset);
+				objects[i] = readLong(data, offset);
 			} else if (classes[i] == Boolean.class) {
 				objects[i] = readBoolean(data, offset);
 			} else if (classes[i] == String.class) {
 				objects[i] = readString(data, offset);
 			} else {
-				throw new IllegalArgumentException("Invalid object type '" + classes[i] + "'. Cannot be deserialized.");
+				throw new SerializationException("Invalid object type '"
+						+ classes[i] + "'. Cannot be deserialized.");
 			}
-			
+
 			// Add to the offset
 			offset += getByteLength(objects[i]);
 		}
-		
+
 		return objects;
 	}
-	
+
+	/**
+	 * This serializes a header byte and a set of objects into a byte array
+	 * which can then be transmit and unserialize via
+	 * {@link ByteStream#unserialize(byte[], Class...)}.
+	 * 
+	 * @param header
+	 *            The header byte to prepend the byte array with (usually the
+	 *            packet type).
+	 * @param objects
+	 *            The objects to serialize
+	 * @return The byte array containing all the objects in serialized form.
+	 * @throws SerializationException
+	 *             if an object was not a serializable type.
+	 */
 	public static byte[] serialize(byte header, Object... objects) {
 		int length = 1; // 1 for object count.
 		for (Object o : objects) {
 			length += getByteLength(o);
 		}
-		
+
 		byte[] data = new byte[length + 3];
 
 		// First put the header byte
 		int offset = 0;
 		offset = writeByte(data, offset, header);
 		offset = writeShort(data, offset, (short) (length));
-		offset = writeByte(data, offset, (byte)objects.length);
+		offset = writeByte(data, offset, (byte) objects.length);
 
 		// Then put the length of the data
 		for (Object o : objects) {
+			// Serialize based on type
 			if (o instanceof Number) {
 				if (o instanceof Byte) {
-					offset = writeByte(data, offset, (Byte)o);
+					offset = writeByte(data, offset, (Byte) o);
 				} else if (o instanceof Short) {
-					offset = writeShort(data, offset, (Short)o);
+					offset = writeShort(data, offset, (Short) o);
 				} else if (o instanceof Integer) {
-					offset = writeInteger(data, offset, (Integer)o);
+					offset = writeInteger(data, offset, (Integer) o);
+				} else if (o instanceof Long) {
+					offset = writeLong(data, offset, (Long) o);
 				} else {
-					throw new IllegalArgumentException("Invalid object type '"
-							+ o.getClass() + "'. Cannot be serialized.");
+					throw new SerializationException(
+							"Invalid object type '" + o.getClass()
+									+ "'. Cannot be serialized.");
 				}
-			} else if (o instanceof Boolean){
-				offset = writeBoolean(data, offset, (Boolean)o);
+			} else if (o instanceof Boolean) {
+				offset = writeBoolean(data, offset, (Boolean) o);
 			} else if (o instanceof String) {
-				offset = writeString(data, offset, (String)o);
+				offset = writeString(data, offset, (String) o);
 			} else {
-				throw new IllegalArgumentException("Invalid object type '"
+				throw new SerializationException("Invalid object type '"
 						+ o.getClass() + "'. Cannot be serialized.");
 			}
 		}
-		
+
 		return data;
 	}
-	
-	public static int getByteLength(Object o) {
+
+	/**
+	 * Calculates the number of bytes required to serialize a given object.
+	 * 
+	 * @param o
+	 *            The object to serialize.
+	 * @return The number of bytes that should be reserved if serializing this
+	 *         object via {@link ByteStream#serialize(byte, Object...)}.
+	 * @throws SerializationException
+	 *             if an object was not a serializable type.
+	 */
+	public static int getByteLength(Object o)
+			throws SerializationException {
 		if (o instanceof Number) {
 			if (o instanceof Byte) {
 				return 1;
@@ -95,12 +153,8 @@ public class ByteStream {
 				return 4;
 			} else if (o instanceof Long) {
 				return 8;
-			} else if (o instanceof Float) {
-				return 4;
-			} else if (o instanceof Double) {
-				return 8;
 			} else {
-				throw new IllegalArgumentException("Invalid object type '"
+				throw new SerializationException("Invalid object type '"
 						+ o.getClass() + "'. Cannot be serialized.");
 			}
 		} else if (o instanceof Boolean) {
@@ -108,7 +162,7 @@ public class ByteStream {
 		} else if (o instanceof String) {
 			return 2 + ((String) o).getBytes(CHARSET).length;
 		} else {
-			throw new IllegalArgumentException("Invalid object type '"
+			throw new SerializationException("Invalid object type '"
 					+ o.getClass() + "'. Cannot be serialized.");
 		}
 	}
@@ -119,13 +173,13 @@ public class ByteStream {
 	}
 
 	public static int writeShort(byte[] bytes, int offset, short data) {
-		bytes[offset] = (byte) (data >> 8);
+		bytes[offset] = (byte) ((data >> 8) & 0xff);
 		bytes[offset + 1] = (byte) (data & 0xff);
 		return offset + 2;
 	}
 
 	public static int writeInteger(byte[] bytes, int offset, int data) {
-		bytes[offset] = (byte) (data >> 24);
+		bytes[offset] = (byte) ((data >> 24) & 0xff);
 		bytes[offset + 1] = (byte) ((data >> 16) & 0xff);
 		bytes[offset + 2] = (byte) ((data >> 8) & 0xff);
 		bytes[offset + 3] = (byte) (data & 0xff);
@@ -143,21 +197,21 @@ public class ByteStream {
 		bytes[offset + 7] = (byte) (data & 0xff);
 		return offset + 8;
 	}
-	
+
 	public static int writeBoolean(byte[] bytes, int offset, boolean data) {
 		if (data) {
-			bytes[offset] = (byte)1;
+			bytes[offset] = (byte) 1;
 		} else {
-			bytes[offset] = (byte)0;
+			bytes[offset] = (byte) 0;
 		}
-		return offset+ 1; 
+		return offset + 1;
 	}
-	
+
 	public static int writeString(byte[] bytes, int offset, String data) {
 		byte[] stringBytes = data.getBytes(CHARSET);
-		writeShort(bytes, offset, (short)stringBytes.length);
+		writeShort(bytes, offset, (short) stringBytes.length);
 		offset += 2;
-		
+
 		for (int i = 0; i < stringBytes.length; i++) {
 			bytes[offset + i] = stringBytes[i];
 		}
@@ -167,21 +221,58 @@ public class ByteStream {
 	public static byte readByte(byte[] bytes, int offset) {
 		return bytes[offset];
 	}
-	
+
 	public static short readShort(byte[] bytes, int offset) {
-		short s = bytes[offset];
-		s <<= 8;
-		s += bytes[offset + 1];
-		return s;
+		short v = bytes[offset];
+		v <<= 8;
+		v |= (bytes[offset + 1] & 0xff);
+		return v;
 	}
-	
-	public static boolean readBoolean(byte[] bytes, int offset){
+
+	public static int readInteger(byte[] bytes, int offset) {
+		int v = bytes[offset];
+		v <<= 8;
+		v |= (bytes[offset + 1] & 0xff);
+		v <<= 8;
+		v |= (bytes[offset + 2] & 0xff);
+		v <<= 8;
+		v |= (bytes[offset + 3] & 0xff);
+		return v;
+	}
+
+	public static long readLong(byte[] bytes, int offset) {
+		long v = bytes[offset];
+		v <<= 8;
+		v |= (bytes[offset + 1] & 0xff);
+		v <<= 8;
+		v |= (bytes[offset + 2] & 0xff);
+		v <<= 8;
+		v |= (bytes[offset + 3] & 0xff);
+		v <<= 8;
+		v |= (bytes[offset + 4] & 0xff);
+		v <<= 8;
+		v |= (bytes[offset + 5] & 0xff);
+		v <<= 8;
+		v |= (bytes[offset + 6] & 0xff);
+		v <<= 8;
+		v |= (bytes[offset + 7] & 0xff);
+		return v;
+	}
+
+	public static boolean readBoolean(byte[] bytes, int offset) {
 		return bytes[offset] == 1;
 	}
-	
+
 	public static String readString(byte[] bytes, int offset) {
 		// Get length
 		short length = readShort(bytes, offset);
-		return new String(Arrays.copyOfRange(bytes, offset + 2, offset + 2 + length + 1), CHARSET);
+		return new String(Arrays.copyOfRange(bytes, offset + 2, offset + 2
+				+ length), CHARSET);
+	}
+
+	public static class SerializationException extends RuntimeException {
+		public SerializationException(String message) {
+			super(message);
+		}
 	}
 }
