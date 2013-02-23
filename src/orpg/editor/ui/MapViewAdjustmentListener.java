a0530c85a45b7c7d15e7f35c9c860c464412f910
jokeofweek <dominiccharleyroy@gmail.com>
2013-02-23 17:47:06 -0500

    Map view now picks out entire range of segments to stream from scrollpane.

--- src/orpg/editor/ui/MapViewAdjustmentListener.java
+++ src/orpg/editor/ui/MapViewAdjustmentListener.java
@@ -24,13 +24,33 @@ public class MapViewAdjustmentListener implements AdjustmentListener {
 
 	@Override
 	public void adjustmentValueChanged(AdjustmentEvent e) {
-		int segmentX = mapController.getSegmentX(scrollPane
-				.getHorizontalScrollBar().getValue()
-				/ (Constants.TILE_WIDTH / editorController.getScaleFactor()));
-		int segmentY = mapController.getSegmentY(scrollPane
-				.getVerticalScrollBar().getValue()
-				/ (Constants.TILE_HEIGHT / editorController.getScaleFactor()));
-		mapController.getRequestManager().requestSegment(segmentX, segmentY);
-	}
+		int tileWidth = (Constants.TILE_WIDTH / editorController
+				.getScaleFactor());
+		int tileHeight = (Constants.TILE_HEIGHT / editorController
+				.getScaleFactor());
 
+		int segmentX = mapController.getSegmentX(scrollPane
+				.getHorizontalScrollBar().getValue() / tileWidth);
+		int segmentY = mapController.getSegmentY(scrollPane
+				.getVerticalScrollBar().getValue() / tileHeight);
+
+		int endSegmentX = (scrollPane.getHorizontalScrollBar().getValue() + (int) scrollPane
+				.getSize().getWidth()) / tileWidth;
+		int endSegmentY = (scrollPane.getVerticalScrollBar().getValue() + (int) scrollPane
+				.getSize().getHeight()) / tileHeight;
+
+		// Don't want end segment to pass map width/height
+		endSegmentX = Math.min(endSegmentX, mapController.getMapWidth() - 1);
+		endSegmentY = Math.min(endSegmentY, mapController.getMapHeight() - 1);
+
+		endSegmentX = mapController.getSegmentX(endSegmentX);
+		endSegmentY = mapController.getSegmentY(endSegmentY);
+
+		for (; segmentX <= endSegmentX; segmentX++) {
+			for (; segmentY <= endSegmentY; segmentY++) {
+				mapController.getRequestManager().requestSegment(segmentX,
+						segmentY);
+			}
+		}
+	}
 }
