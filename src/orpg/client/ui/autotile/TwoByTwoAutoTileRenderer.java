0054843020cf549b2d9d376374e05dcc706735f7
jokeofweek <dominiccharleyroy@gmail.com>
2013-03-03 20:42:17 -0500

    Renamed to fix casing issue.

--- src/orpg/client/ui/autotile/TwoByTwoAutoTileRenderer.java
+++ src/orpg/client/ui/autotile/TwoByTwoAutoTileRenderer.java
@@ -2,7 +2,7 @@ package orpg.client.ui.autotile;
 
 import java.util.HashMap;
 
-import orpg.editor.ui.autotile.TwoByThreeAutoTileRenderer;
+import orpg.editor.ui.autotile.TwoByThreeAutoTileRenderer2;
 import orpg.shared.Constants;
 import orpg.shared.data.Map;
 
