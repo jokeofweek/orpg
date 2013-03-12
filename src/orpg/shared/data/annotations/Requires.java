b8677a7e17dfd7df9fefd5594b362ecda2824912
jokeofweek <dominiccharleyroy@gmail.com>
2013-03-12 08:44:35 -0400

    Added Requires annotation.

---
+++ src/orpg/shared/data/annotations/Requires.java
@@ -0,0 +1,11 @@
+package orpg.shared.data.annotations;
+
+import java.lang.annotation.*;
+
+import com.artemis.Component;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Requires {
+	Class<? extends Component> dependency();
+}
